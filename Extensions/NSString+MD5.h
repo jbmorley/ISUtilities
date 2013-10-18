7983b3de1ad195b65e0109eacc29975a83dbceb9
Jason Barrie Morley <jason.morley@inseven.co.uk>
2013-10-18 18:31:10 +0100

    Adding some NSString extneions.

---
+++ Extensions/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString+MD5.h
+//  Popcorn
+//
+//  Created by Jason Barrie Morley on 12/08/2013.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+
+- (NSString *)MD5;
+
+@end
