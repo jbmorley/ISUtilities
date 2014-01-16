9d395c42f17e4b84d237217a0d8eff78e6b7c17d
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-01-16 10:43:36 +0100

    Ensuring the MIT license is on the source files.

--- Classes/UIAlertView+BlockExtensions.m
+++ Classes/UIAlertView+BlockExtensions.m
@@ -1,12 +1,42 @@
+//
+// Copyright (c) 2013 InSeven Limited.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
 #import "UIAlertView+BlockExtensions.h"
 #import <objc/runtime.h>
 
 @implementation UIAlertView (BlockExtensions)
 
-- (id)initWithTitle:(NSString *)title message:(NSString *)message completionBlock:(void (^)(NSUInteger buttonIndex))block cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitles:(NSString *)otherButtonTitles, ... {
+- (id)initWithTitle:(NSString *)title
+            message:(NSString *)message
+    completionBlock:(void (^)(NSUInteger buttonIndex))block
+  cancelButtonTitle:(NSString *)cancelButtonTitle
+  otherButtonTitles:(NSString *)otherButtonTitles, ... {
 
   objc_setAssociatedObject(self, "blockCallback", (__bridge id)Block_copy((__bridge void *)block), OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-	if (self = [self initWithTitle:title message:message delegate:self cancelButtonTitle:nil otherButtonTitles:nil]) {
+	if (self = [self initWithTitle:title
+                         message:message
+                        delegate:self
+               cancelButtonTitle:nil
+               otherButtonTitles:nil]) {
     
 		if (cancelButtonTitle) {
 			[self addButtonWithTitle:cancelButtonTitle];
