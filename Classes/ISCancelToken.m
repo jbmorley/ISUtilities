99bae8f41df17afd9fdf2baec018d9ac1b6feab6
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-05-05 15:30:36 +0200

    Adding the ISCancelToken to ISUtilities.

--- Classes/ISCancelToken.m
+++ Classes/ISCancelToken.m
@@ -7,10 +7,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -18,17 +18,61 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-// 
+//
 
-#import <Foundation/Foundation.h>
-
-#import "ISNotifier.h"
-#import "NSDictionary+JSON.h"
-#import "NSObject+Serialize.h"
-#import "UIAlertView+Block.h"
-#import "UIApplication+Activity.h"
-#import "UIApplication+IdleTimer.h"
-#import "UIView+Utilities.h"
-#import "UINavigationController+Pops.h"
-#import "NSObject+Threads.h"
 #import "ISCancelToken.h"
+#import "ISNotifier.h"
+
+@interface ISCancelToken () {
+  BOOL _cancelled;
+}
+
+@property (nonatomic, strong) ISNotifier *notifier;
+
+@end
+
+@implementation ISCancelToken
+
+
+- (id)init
+{
+  self = [super init];
+  if (self) {
+    self.notifier = [ISNotifier new];
+  }
+  return self;
+}
+
+
+- (void)cancel
+{
+  @synchronized(self) {
+    if (!_cancelled) {
+      _cancelled = YES;
+      [self.notifier notify:@selector(tokenDidCancel)];
+    }
+  }
+}
+
+
+- (BOOL)isCancelled
+{
+  @synchronized(self) {
+    return _cancelled;
+  }
+}
+
+
+- (void)addObserver:(id)observer
+{
+  [self.notifier addObserver:observer];
+}
+
+
+- (void)removeObserver:(id)observer
+{
+  [self.notifier removeObserver:observer];
+}
+
+
+@end
