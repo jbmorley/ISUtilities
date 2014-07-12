69545ec1679f55db2bc0bb4c8b1d824aeb697a62
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-07-12 16:12:53 +0200

    Adding the empty tests project.

---
+++ Tests/ISUtilitiesTests/ISUtilitiesTests.m
@@ -0,0 +1,34 @@
+//
+//  ISUtilitiesTests.m
+//  ISUtilitiesTests
+//
+//  Created by Jason Barrie Morley on 12/07/2014.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ISUtilitiesTests : XCTestCase
+
+@end
+
+@implementation ISUtilitiesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
