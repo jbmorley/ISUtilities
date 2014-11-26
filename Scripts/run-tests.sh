c86a9e4d24d8771f8d0a65046c4c8ddc71e4fcce
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-26 09:51:07 +0100

    The tests now run.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 set -e
+set -u
 
 pushd Tests/iOS
-
 pod install
-xcodebuild \
-    -sdk iphonesimulator8.0
-    -workspace ISUtilitiesTests.xcworkspace \
-    -scheme ISUtilitiesTests \
-    clean test || exit 1
-
+xcodebuild -sdk iphonesimulator8.0 -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test || exit 1
 popd
 
 pushd Tests/OSX
-
 pod install
-xcodebuild \
-    -workspace ISUtilitiesTests.xcworkspace \
-    -scheme ISUtilitiesTests \
-    clean test || exit 1
-
+xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test || exit 1
 popd
