5ae8e904908e6d4c24dd1d0b3fa3c1005831c6bb
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-26 18:25:25 +0100

    Omitting the version from the docs script to reduce the number of things which need updating upon release.

--- Scripts/build-docs.sh
+++ Scripts/build-docs.sh
@@ -4,7 +4,6 @@ appledoc \
     --project-company "InSeven Limited" \
     --company-id "uk.co.inseven" \
     --project-name ISUtilities \
-    --project-version "1.1.0" \
     --output Documentation \
     --create-docset \
     --index-desc README.md \
