e6a2f9f506f172c1047119ada143ee07ff35710b
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-26 09:37:02 +0100

    Unpinning the iOS SDK used for the tests.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -5,8 +5,7 @@ set -e
 pushd Tests/iOS
 
 pod install
-xcodebuild -sdk iphonesimulator8.0 \
-           -workspace ISUtilitiesTests.xcworkspace \
+xcodebuild -workspace ISUtilitiesTests.xcworkspace \
            -scheme ISUtilitiesTests \
            test || exit 1
 
