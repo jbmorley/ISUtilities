e51a302b18669584c72dbbb3aa370f17ed0ba71d
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-26 10:04:05 +0100

    Updating the tests to explicitly select an iPhone 6 simulator for testing.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -5,7 +5,7 @@ set -u
 
 pushd Tests/iOS
 pod install
-xcodebuild -sdk iphonesimulator8.0 -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test || exit 1
+xcodebuild -sdk iphonesimulator8.0 -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test || exit 1
 popd
 
 pushd Tests/OSX
