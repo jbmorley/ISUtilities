7ecdb7ce3537433071bc923d13975897df9dad7e
Jason Barrie Morley <jason.morley@inseven.co.uk>
2014-11-26 16:56:44 +0100

    Moving the ISNotification documentation into the header.

--- ISUtilities/ISNotifier.h
+++ ISUtilities/ISNotifier.h
@@ -22,11 +22,56 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * A lightweight notificaion mechanism for observers for situations where NSNotificationCenter requires too much
+ * boiler-plate code or isn't explicit enough:
+ *
+ *
+ * ```
+ * #import <ISUtilities/ISUtilities.h>
+ *
+ * // Construct the notifier.
+ * ISNotifier *notifier = [ISNotifier new];
+ *
+ * // Add an observer.
+ * id anObserver = [YourCustomClass new];
+ * [notifier addObserver:anObserver];
+ *
+ * // Notifying all observers.
+ * [notifier notify:@selector(didUpdate:) withObject:self];
+ *
+ * // Remove the observer (optional).
+ * [notifier removeObserver:anObserver];
+ * ```
+ *
+ * Notes:
+ *
+ * - Observers are weakly referenced so it is not necessary to remove them when observers are released.
+ * - Notifications are dispatched to all observers which respond to a given selector using the
+ *   `notify:withObject:withObject:...` methods.
+ * - Notifications are called on the same thread as the call to `notify:`, `notify:withObject:`, etc.
+ * - It is recommended that you wrap the calls to `addObserver:` and `removeObserver:` with ones which enforce a
+ *   protocol to avoid adding the wrong type of class or simply failing to implement one of your observer selectors.
+ * - ISNotifier is not thread-safe with one exception: observers can be safely released on any thread.
+ */
 @interface ISNotifier : NSObject
 
 @property (nonatomic, readonly) NSUInteger count;
 
+/**
+ * Add an observer.
+ * 
+ * The observer is weakly referenced and not retained.
+ *
+ * @param observer The observer to add.
+ */
 - (void)addObserver:(id)observer;
+
+/**
+ * Remove an observer.
+ *
+ * @param observer The observer to remove.
+ */
 - (void)removeObserver:(id)observer;
 
 - (void)notify:(SEL)selector;
--- README.md
+++ README.md
@@ -31,41 +31,6 @@ Tests can be run by executing the following command from the root of the reposit
 ./Scripts/run-tests.sh
 ```
 
-Classes
--------
-
-### ISNotifier
-
-A lightweight notificaion mechanism for observers for situations where NSNotificationCenter requires too much boiler-plate code or isn't explicit enough:
-
-
-```objc
-#import <ISUtilities/ISUtilities.h>
-    
-// Construct the notifier.
-ISNotifier *notifier = [ISNotifier new];
-
-// Add an observer.
-id anObserver = [YourCustomClass new];
-[notifier addObserver:anObserver];
-
-// Notifying all observers.
-[notifier notify:@selector(didUpdate:) 
-      withObject:self];
-
-// Remove the observer (optional).
-[notifier removeObserver:anObserver];
-```
-
-Notes:
-
-- Observers are added and removed with the `addObserver:` and `removeObserver:` methods.
-- Observers are weakly referenced so it is not necessary to remove them when observers are released.
-- Notifications are dispatched to all observers which respond to a given selector using the `notify:withObject:withObject:...` methods. 
-- It is recommended that you wrap the calls to `addObserver:` and `removeObserver:` with ones which enforce a protocol to avoid adding the wrong type of class or simply failing to implement one of your observer selectors.
-- ISNotifier is not thread-safe.
-
-
 ### NSDictionary+JSON
 
 JSON serialization and de-serialization category for NSDictionary:
