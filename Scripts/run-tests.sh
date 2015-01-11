7b0561ac2d81581a9ded4b4a715f4f86fa66b930
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 21:13:03 +0000

    Dropping xcpretty.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -7,12 +7,12 @@ set -u
 
 pushd Tests/iOS > /dev/null
 echo "Running iOS Tests..."
-pod update --silent
-xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test | xcpretty -c
+pod update
+xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test
 popd > /dev/null
 
 pushd Tests/OSX > /dev/null
 echo "Running OS X Tests..."
-pod update --silent
-xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test | xcpretty -c
+pod update
+xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test
 popd > /dev/null
