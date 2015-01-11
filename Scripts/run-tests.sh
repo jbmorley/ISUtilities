1c8fd7cdc33ad2574ee8ade6dec894112c94f087
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 19:53:08 +0000

    Removing a failed attempt at a new lime.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -6,13 +6,13 @@ set -e
 set -u
 
 pushd Tests/iOS > /dev/null
-echo "Running iOS Tests...\n"
+echo "Running iOS Tests..."
 pod update --silent
 xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test | xcpretty -c
 popd > /dev/null
 
 pushd Tests/OSX > /dev/null
-echo "Running OS X Tests...\n"
+echo "Running OS X Tests..."
 pod update --silent
 xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test | xcpretty -c
 popd > /dev/null
