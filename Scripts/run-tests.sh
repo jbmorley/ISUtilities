8f138fbb07dc7fe63ea7e60af639bc745e053a77
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 19:46:28 +0000

    Silencing CocoaPods.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -5,12 +5,14 @@ simulator_sdk="iphonesimulator8.1"
 set -e
 set -u
 
-pushd Tests/iOS
-pod update
+pushd Tests/iOS > /dev/null
+echo "Running iOS Tests...\n"
+pod update --silent
 xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test | xcpretty -c
-popd
+popd > /dev/null
 
-pushd Tests/OSX
-pod update
+pushd Tests/OSX > /dev/null
+echo "Running OS X Tests...\n"
+pod update --silent
 xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test | xcpretty -c
-popd
+popd > /dev/null
