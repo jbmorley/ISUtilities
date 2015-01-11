79040118683ec2403e9bbe173ad1c875ce8a7e8f
Jason Barrie Morley <jason.morley@inseven.co.uk>
2015-01-11 21:47:37 +0000

    Re-adding xcpretty in a cleaner way.

--- Scripts/run-tests.sh
+++ Scripts/run-tests.sh
@@ -7,12 +7,16 @@ set -u
 
 pushd Tests/iOS > /dev/null
 echo "Running iOS Tests..."
-pod update
-xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean test
+echo "Updating CocoaPods..."
+pod update --silent
+xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" clean build | xcpretty -c
+xcodebuild -sdk "$simulator_sdk" -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests -destination "name=iPhone 6" test
 popd > /dev/null
 
 pushd Tests/OSX > /dev/null
 echo "Running OS X Tests..."
-pod update
-xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean test
+echo "Updating CocoaPods..."
+pod update --silent
+xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests clean build | xcpretty -c
+xcodebuild -workspace ISUtilitiesTests.xcworkspace -scheme ISUtilitiesTests test
 popd > /dev/null
